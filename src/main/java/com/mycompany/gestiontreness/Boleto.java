0bbd930bf7c200c39c28d708cc4e791765427c68
Jhostyn Gualdrón <chae.mina2024@gmail.com>
2025-02-22 22:16:23 -0500

    Modificación archivo Boleto, se añadió un atributo

--- src/main/java/com/mycompany/gestiontreness/Boleto.java
+++ src/main/java/com/mycompany/gestiontreness/Boleto.java
@@ -4,10 +4,34 @@
  */
 package com.mycompany.gestiontreness;
 
+import java.util.Objects;
+
 /**
  *
  * @author jhost
  */
 public class Boleto {
-    
+    private String nombre;
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Boleto other = (Boleto) obj;
+        return Objects.equals(this.nombre, other.nombre);
+    }
+         
 }
