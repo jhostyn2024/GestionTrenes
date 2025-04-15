9cf7a4826c7bd43131baec1550b715fac21cf022
Jhostyn Gualdrón <chae.mina2024@gmail.com>
2025-04-15 17:59:36 -0500

    XD

--- src/main/java/com/mycompany/gestiontreness/Empleado.java
+++ src/main/java/com/mycompany/gestiontreness/Empleado.java
@@ -2,11 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package com.mycompany.gestiontreness;
 
 /**
  *
  * @author jhost
  */
-public class AdminGUI {
+public class Empleado {
     
 }
